ae0be6d652537b7bb18b792c739cb9e0488074e7
xiaobin.wang <1367718597@qq.com>
2018-03-14 17:48:58 +0800

    inti

--- src/main/java/com/test/controller/Hello.java
+++ src/main/java/com/test/controller/Hello.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -35,5 +38,10 @@ public class Hello {
     @RequestMapping("/findall")
     public List<Category> getCategory() {
         return categoryService.findAll();
+    }
+
+    @RequestMapping("/upload")
+    public void upload(MultipartFile file) throws IOException {
+        file.transferTo(new File("D;//12.txt"));
     }
 }
